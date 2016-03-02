dcfb94cbae849070bff1e3522046f9124e5912ac
Ilina <ilina@ers-japan.com>
2016-03-02 12:03:36 +0800

    coupon edited

--- fuel/app_admin/views/coupon/input.php
+++ fuel/app_admin/views/coupon/input.php
@@ -1,38 +1,32 @@
     <!-- Main content -->
-    <section class="content">
     <?php echo Form::open(array('method' => 'post')); ?>
-
+      <section class="content">
 					    <div class="form-group">
                   <label for="exampleInputEmail1">Shop ID</label>
                   <input type="text" class="form-control" name="id" id="exampleInputEmail1" placeholder="Shop ID">
-                </div>
-
-
-                    <div class="form-group">
+              </div>
+              <div class="form-group">
                   <label for="exampleInputEmail1">Coupon Title</label>
                   <input type="text" class="form-control" name="title" id="exampleInputEmail1" placeholder="Coupon Title">
-                </div>
-
-                  <div class="form-group">
+              </div>
+              <div class="form-group">
                   <label for="exampleInputFile">Coupon thumbnail</label>
                   <input type="file" name="thumbnail" id="exampleInputFile">
-                </div>
-
-          <div class="box">
-            <div class="box-header">
+              </div>
+              
+              <div class="box">
+              <div class="box-header">
               <h3 class="box-title">Coupon Description
               </h3>
-              <!-- tools box -->
+
               <div class="pull-right box-tools">
                 <button type="button" class="btn btn-default btn-sm" data-widget="collapse" data-toggle="tooltip" title="Collapse">
                   <i class="fa fa-minus"></i></button>
                 <button type="button" class="btn btn-default btn-sm" data-widget="remove" data-toggle="tooltip" title="Remove">
                   <i class="fa fa-times"></i></button>
               </div>
-              <!-- /. tools -->
             </div>
 
-            <!-- /.box-header -->
             <div class="box-body pad">
               <form>
                 <textarea class="textarea" name="description" placeholder="Place some text here" style="width: 100%; height: 200px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;"></textarea>
@@ -52,29 +46,21 @@
               <br>
               <input class="form-control input-sm" type="text" name="Conditions4" placeholder="Conditions4">
             </div>
-            <!-- /.box-body -->
           </div>  
           </div>
 
-                <div class="form-group">
-                <label>Expiration date(Day)</label>
-
-                <div class="input-group">
-                  <div class="input-group-addon">
-                    <i class="fa fa-calendar"></i>
-                  </div>
-                  <input type="text" class="form-control" name="expirationdate" data-inputmask="'alias': 'dd/mm/yyyy'" data-mask>
-                </div>
-                <!-- /.input group -->
+          <div class="form-group">
+            <label>Expiration date(Day)</label>
+              <div class="input-group">
+              <div class="input-group-addon">
+                <i class="fa fa-calendar"></i>
               </div>
-
-
-               <div class="box-footer">
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <input type="text" class="form-control" name="expirationdate" data-inputmask="'alias': 'dd/mm/yyyy'" data-mask>
               </div>
-        <!-- /.col-->
-         <form>
-     <?php echo Form::close(); ?>
-      <!-- ./row -->
-    </section>
-    <!-- /.content -->
+              </div>
+          
+          <div class="box-footer">
+            <input type="submit" name="submit" id="submit" class="btn btn-primary" value="Submit"/>
+          </div>
+      </section>
+    <?php echo Form::close(); ?>
