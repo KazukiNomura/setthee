20a4aea418bd2e9f362ca70cded2959ef60036a4
harukawa <harukawa@beyond-inc.jp>
2016-04-28 16:09:46 +0900

    環境設定

--- fuel/app_admin/config/staging/db.php
+++ fuel/app_admin/config/staging/db.php
@@ -1,14 +1,44 @@
 <?php
 /**
- * The staging database settings. These get merged with the global settings.
+ * The development database settings. These get merged with the global settings.
  */
 
 return array(
-	'default' => array(
-		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=fuel_staging',
-			'username'   => 'fuel_app',
-			'password'   => 'super_secret_password',
-		),
-	),
+    'active' => 'ERSdb',
+    /**
+     * Base config, just need to set the DSN, username and password in env. config.
+     */
+    'default' => array(
+        'type'        => 'pdo',
+        'connection'  => array(
+            'persistent' => false,
+        ),
+        'identifier'   => '`',
+        'table_prefix' => '',
+        'charset'      => 'utf8',
+        'enable_cache' => true,
+        'profiling'    => false,
+    ),
+
+    'redis' => array(
+        'default' => array(
+            'hostname'  => '127.0.0.1',
+            'port'      => 6379,
+        )
+    ),
+
+    'ERSdb' => array(
+        'type'   => 'mysqli',
+        'connection' => array(
+            'hostname'   => 'localhost',
+            'database'   => 'setthee',
+            'username'   => 'root',
+            'password'   => 'setthee2016',
+            'persistent' => FALSE,
+        ),
+        'table_prefix' => '',
+        'charset'      => 'utf8',
+        'caching'      => false,
+        'profiling'    => true,
+    ),
 );
