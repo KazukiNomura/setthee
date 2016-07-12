bbca8b96df0a5995f7bb0973d0a604872aaf583c
KazukiNomura <keen.kiss.cross@gmail.com>
2016-07-12 19:14:00 +0900

    detail.phpの修正

--- fuel/app/views/shop/detail.php
+++ fuel/app/views/shop/detail.php
@@ -1,4 +1,4 @@
-     <div id="wrapper">
+ <div id="wrapper">
         <div class="wrapperIn">
         <header class="header clearfix">
             <ul class="gNav">
@@ -7,125 +7,41 @@
                 <li class="gNav03"><a href="#"><img src="/design/common/img/header/menu.png" alt="" /></a></li>
             </ul>
         </header><!-- header -->
- 　　　　<p class="titname iconarr">Category</p>
-        <div class="content clearfix">
-            <div class="shopdetail">
-            <div class="shopsec clearfix">
-            <ul class="shop_l"> 
-                <li class="shop_tlt"><?=$shop_info['shop_name'];?></li>
-                <li class="shop_txt"><?=$shop_info['address'];?></li>
-                <li><span class="shopIcon01"><img src="/design/imges/shopdetail/shopdetail_02.png" alt="img" /><?=$shop_info['price'];?></span><span class="shopIcon02"><img src="/design/imges/shopdetail/shopdetail_03.png" alt="img" /><?=$shop_info['price'];?></span></li>
-            </ul>
-            <p class="shop_r"><img src="/design/imges/shopdetail/shopdetail_01.png" alt="img" /></p>
-            </div>
-             <ul class="shopAdd clearfix">
-                <li><a href="#"><img src="/design/imges/shopdetail/shopdetail_tel.png" alt="tel" /></a></li>
-                <li><a href="#"><img src="/design/imges/shopdetail/shopdetail_map.png" alt="map" /></a></li>
-            </ul>
-            <div class="shopsec01 clearfix">
-                <img src="/design/imges/shopdetail/shopdetail_baner.png" alt="thumbnail" />
-                <p class="shoppara"><?=$shop_info['description'];?></p>
-            </div>
-            <p class="shoptable_tlt">Information</p>
-            <table class="shopdetail_tb">
-                <tr>
-                    <th>Shop name</th>
-                    <td><?=$shop_info['shop_name'];?></td>
-                </tr>
-                <tr>
-                    <th>Category</th>
-                    <td>Category</td>
-                </tr>
-                <tr>
-                    <th>Menu</th>
-                    <td><a href="#" class="viewmenu">view menu</a></td>
-                </tr>
-                <tr>
-                    <th>Address</th>
-                    <td><?=$shop_info['address'];?></td>
-                </tr>
-                <tr>
-                    <th>Open/Close</th>
-                    <td>texttexttexttexttexttext</td>
-                </tr>
-                <tr>
-                    <th>Cost average</th>
-                    <td><?=$shop_info['price'];?></td>
-                </tr>
-                <tr>
-                    <th>Credit card</th>
-                    <td><?=$shop_info['credit_id'];?></td>
-                </tr>
-                <tr>
-                    <th>Phone</th>
-                    <td><?=$shop_info['phone_number'];?></td>
-                </tr>
-                <tr>
-                    <th>WiFi</th>
-                    <td><?=$shop_info['wi-fi'];?></td>
-                </tr>
-            </table>
-            <div class="shopdetail_photo clearfix">
-                <p class="photo_tlt">Photos</p>
-                <ul class="shopphoto_ul clearfix">
-                    <li><img src="/design/imges/shopdetail/shopdetail_04.png" alt="img" /></li>
-                    <li><img src="/design/imges/shopdetail/shopdetail_04.png" alt="img" /></li>
-                    <li><img src="/design/imges/shopdetail/shopdetail_04.png" alt="img" /></li>
-                    <li class="shopphoto_li"><img src="/design/imges/shopdetail/shopdetail_04.png" alt="img" /></li>
-                </ul>
-                <ul class="shopphoto_ul clearfix">
-                    <li><img src="/design/imges/shopdetail/shopdetail_04.png" alt="img" /></li>
-                    <li><img src="/design/imges/shopdetail/shopdetail_04.png" alt="img" /></li>
-                    <li><img src="/design/imges/shopdetail/shopdetail_04.png" alt="img" /></li>
-                    <li class="shopphoto_li"><img src="/design/imges/shopdetail/shopdetail_04.png" alt="img" /></li>
-                </ul>
-                <p class="viewmore"><a href="#" class="viewbtn">view more</a></p>
-                
-            </div>
-            <div class="shopdetail_coupon clearfix">
-                <p class="photo_tlt">Coupon</p>
-                <div class="shopcoupontit">
-                <h1 class="coupon_tlt">Coupon title01</h1>
-                    <ul class="couponpara clearfix">
-                        <li class="coupon_img"><img src="/design/imges/couponlist/couponlist_img01.png"></li>
-                        <li class="coupon_sp02"><span class="coupon_sp03">commentcommentcommentcommentcommentcommentcommentcommentcommentcommentcommentcommentcommentcommentcommentcommentcommentcomment</span></li>
-                    </ul>
-                    <p class="shopNotetit">Notes</p>
-                    <p class="shopnotetxt">commentcommentcommentcommentcommentcommentcommentcommentcommentcommentcommentcommentcommentcommentcommentcommentcommentcomment</p>
-                </div><!-- shopcoupontit -->
-                <div class="shopcoupontit">
-                <h1 class="coupon_tlt">Coupon title02</h1>
-                    <ul class="couponpara clearfix">
-                        <li class="coupon_img"><img src="/design/imges/couponlist/couponlist_img01.png"></li>
-                        <li class="coupon_sp02"><span class="coupon_sp03">commentcommentcommentcommentcommentcommentcommentcommentcommentcommentcommentcommentcommentcommentcommentcommentcommentcomment</span></li>
-                    </ul>
-                    <p class="shopNotetit">Notes</p>
-                    <p class="shopnotetxt">commentcommentcommentcommentcommentcommentcommentcommentcommentcommentcommentcommentcommentcommentcommentcommentcommentcomment</p>
-                </div><!-- shopcoupontit -->
-                <div class="shopcoupontit">
-                <h1 class="coupon_tlt">Coupon title03</h1>
-                    <ul class="couponpara clearfix">
-                        <li class="coupon_img"><img src="/design/imges/couponlist/couponlist_img01.png"></li>
-                        <li class="coupon_sp02"><span class="coupon_sp03">commentcommentcommentcommentcommentcommentcommentcommentcommentcommentcommentcommentcommentcommentcommentcommentcommentcomment</span></li>
-                    </ul>
-                    <p class="shopNotetit">Notes</p>
-                    <p class="shopnotetxt">commentcommentcommentcommentcommentcommentcommentcommentcommentcommentcommentcommentcommentcommentcommentcommentcommentcomment</p>
-                </div><!-- shopcoupontit -->
-            </div>
-            <p class="shopreport">
-                <a href="#"><img src="/design/imges/shopdetail/shop_report.png" alt="" /></a>
-            </p>
-            
-           
+        <div class="banner">
+            <img src="design/imges/top/top_bnr.png" alt="" />
         </div>
-        </div><!-- content -->
+        <div class="menu clearfix">
+            <ul class="menuIn">
+                <li class="menu02 active"><a href="#">New SHOP</a></li>
+            </ul>
+        </div><!-- menu -->
+        <div class="content clearfix top">
+            <div class="contentIn">
+                <div class="topsec clearfix">
 
+                <?php foreach ($list as $info): ?>
+                    <ul class="topsecLeft">
+                        <li class="comment"><a href="/shop/detail?id=<?=$info['id'];?>"><?=$info['shop_name'];?></a></li>
+                   　　　<li class="topthumbnail"><img src="design/imges/top/top_img02.png" alt="" /></li>
+                        <li class="text2"><?=$info['businesshours'];?></li>
+                        <li class="text"><?=$info['address_info'];?></li>
+                        <li class="place"><?=$info['price'];?></li>
+                    </ul>
+
+
+                    <?php endforeach; ?>
+
+
+                </div><!-- topsec -->
+
+                </div><!-- topsec -->
+            </div>
+        </div><!-- content -->
         <ul class="footer clearfix">
-                <li><a href="#"><img src="/design/common/img/footer/ft_img01_off.png" alt="" /></a></li>
+                <li><a href="#" class="fnav01"><img src="/design/common/img/footer/ft_img01_off.png" alt="" /></a></li>
                 <li><a href="#"><img src="/design/common/img/footer/ft_img02_off.png" alt="" /></a></li>
-                <li><a href="#"><img src="/design/common/img/footer/ft_img03_on.png" alt="" /></a></li>
+                <li><a href="#"><img src="/design/common/img/footer/ft_img03_off.png" alt="" /></a></li>
                 <li><a href="#"><img src="/design/common/img/footer/ft_img04_off.png" alt="" /></a></li>
         </ul><!-- footer -->
         </div><!-- wrapperIn -->
     </div><!-- wrapper -->
-
